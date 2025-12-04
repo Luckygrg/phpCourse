0f8e3464101890ebec60e653f33e48e89d2a87d7
Lucky Gurung <luckygrxx@gmail.com>
2025-12-04 11:57:03 +0545

    first php website

---
+++ Auth/signup.php
@@ -0,0 +1,8 @@
+<?php
+include '../layouts/header.php';
+include '../layouts/nav.php'; 
+?>
+
+<?php
+include '../layouts/footer.php'; 
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+include 'layouts/header.php';
+include 'layouts/nav.php';
+
+?>
+
+
+<main>
+    <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/images/pic1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/pic2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/pic3.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</main>
+<?php
+include 'layouts/footer.php';
+?>
